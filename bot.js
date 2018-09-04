cc8b6420ce7bca71faaed851f5a522b78e6da2cd
BerkCsknn <42947274+BerkCsknn@users.noreply.github.com>
2018-09-05 01:26:27 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -81,7 +81,7 @@ client.unload = command => {
 client.on('message', msg => {
   if (msg.content.toLowerCase() === 'sa') {
 		if (!msg.guild.member(msg.author).hasPermission("BAN_MEMBERS")) {
-			msg.author.sendMessage('Aleyküm selam,  hoş geldin ^^'); 
+			msg.channel.send('Aleyküm selam,  hoş geldin ^^'); 
 		} else {
 		msg.channel.send('Aleyküm selam, hoş geldin ^^');
 		}
