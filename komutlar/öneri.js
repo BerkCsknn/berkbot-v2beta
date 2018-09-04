b6c5ae9c8f27b79118b3153974a54adc6fa04aff
BerkCsknn <42947274+BerkCsknn@users.noreply.github.com>
2018-09-05 01:29:28 +0300

    Update öneri.js

--- komutlar/öneri.js
+++ komutlar/öneri.js
@@ -19,7 +19,7 @@ exports.run = function(client, message, args) {
 			.addField("Öneri", öneri)
 		
 		client.guilds.get(guildID).channels.get(channelID).send(embed);
-		msg.author.sendMessage("Öneriniz alınmıştır! Teşekkür ederiz.");
+		msg.reply("Öneriniz alınmıştır! Teşekkür ederiz.");
 	};
 
 
