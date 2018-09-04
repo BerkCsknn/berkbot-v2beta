2596dc5febd4388b8fd3316d9c6761785d752d8e
BerkCsknn <42947274+BerkCsknn@users.noreply.github.com>
2018-09-05 01:47:00 +0300

    Update ig.js

--- komutlar/ig.js
+++ komutlar/ig.js
@@ -5,7 +5,7 @@ exports.run = function(client, message) {
  
     const embed = new Discord.RichEmbed()
         .setDescription("**ISTIKLAL GAMING**")
-        .setImage("https://cdn.discordapp.com/icons/462629397644443668/02f06aa5ae63941fd9dfaf66fc16ec69.jpg")
+        .setImage("https://i.hizliresim.com/7DJQ7W.jpg")
         .setThumbnail("https://cdn.discordapp.com/icons/462629397644443668/02f06aa5ae63941fd9dfaf66fc16ec69.jpg")
         .setColor(0x00AE86)
         .addField("Yönetici", "Serkan Tuna", true)
